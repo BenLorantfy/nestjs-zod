116d4d7141fc6e18372790bb2cfeffc95b8a156a
Evgeny Zakharov <risenforces@gmail.com>
2022-05-17 22:09:47 +0300

    fix: make json schema callable

--- src/z/generic-types/json.ts
+++ src/z/generic-types/json.ts
@@ -5,6 +5,14 @@ export type Json = Literal | { [key: string]: Json } | Json[]
 
 const literal = z.union([z.string(), z.number(), z.boolean()])
 
-export const json: ZodSchema<Json> = z.lazy(() =>
-  z.union([literal, z.array(json), z.record(json)])
-)
+const DEFAULT_MESSAGE = 'Expected value to be a JSON-serializable'
+
+export const json = (message: string = DEFAULT_MESSAGE) => {
+  const schema: ZodSchema<Json> = z.lazy(() =>
+    z.union([literal, z.array(schema), z.record(schema)], {
+      invalid_type_error: message,
+    })
+  )
+
+  return schema
+}
