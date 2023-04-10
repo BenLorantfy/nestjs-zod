a0c0f1812dd4eedce32d56494a8137d66f1b7f4e
Eyal Cherevatzki <eyal.ch@icloud.com>
2023-04-10 18:57:00 +0300

    fix: fix `validate` function to return the correct type

--- src/validate.ts
+++ src/validate.ts
@@ -1,10 +1,14 @@
-import { ZodSchema } from 'zod'
+import { ZodSchema, ZodTypeDef } from 'zod'
 import { isZodDto, ZodDto } from './dto'
 import { createZodValidationException, ZodExceptionCreator } from './exception'
 
-export function validate(
+export function validate<
+  TOutput = any, // eslint-disable-line @typescript-eslint/no-explicit-any
+  TDef extends ZodTypeDef = ZodTypeDef,
+  TInput = TOutput
+>(
   value: unknown,
-  schemaOrDto: ZodSchema | ZodDto,
+  schemaOrDto: ZodSchema<TOutput, TDef, TInput> | ZodDto<TOutput, TDef, TInput>,
   createValidationException: ZodExceptionCreator = createZodValidationException
 ) {
   const schema = isZodDto(schemaOrDto) ? schemaOrDto.schema : schemaOrDto
