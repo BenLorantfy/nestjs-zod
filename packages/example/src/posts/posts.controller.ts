35e2cd8f492de4536e7a35684fb0803fef258d5d
Ben <BenLorantfy@users.noreply.github.com>
2024-10-16 23:47:07 -0400

    wip

--- packages/example/src/posts/posts.controller.ts
+++ packages/example/src/posts/posts.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 import { createZodDto } from 'nestjs-zod'
 import { z } from 'zod'
 
@@ -13,5 +14,21 @@ export class PostsController {
     @Post()
     createPost(@Body() body: PostDto) {
         return body;
+    }
+
+    @Get()
+    @ApiOkResponse({ type: [PostDto], description: 'Get all posts' })
+    getAll() {
+      return [];
+    }
+
+    @Get(':id')
+    @ApiOkResponse({ type: PostDto, description: 'Get a post by ID' })
+    getById(@Param('id') id: string) {
+      return {
+        title: 'Hello',
+        content: 'World',
+        authorId: 1,
+      };
     }
 }
