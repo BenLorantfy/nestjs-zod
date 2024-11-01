c2f5d37a76695a97dc584e927f1bb01f3e62c963
Ben <BenLorantfy@users.noreply.github.com>
2024-11-01 11:23:16 -0400

    wip

--- packages/example/src/http-exception.filter.ts
+++ packages/example/src/http-exception.filter.ts
@@ -1,25 +1,17 @@
-import { Logger, ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Logger, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
 import { ZodSerializationException } from 'nestjs-zod';
 
 @Catch(HttpException)
-export class HttpExceptionFilter implements ExceptionFilter {
+export class HttpExceptionFilter extends BaseExceptionFilter {
     private readonly logger = new Logger(HttpExceptionFilter.name);
 
     catch(exception: HttpException, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse<Response>();
-        const status = exception.getStatus();
-
         if (exception instanceof ZodSerializationException) {
             const zodError = exception.getZodError();
             this.logger.error(`ZodSerializationException: ${zodError.message}`);
         }
 
-        response
-            .status(status)
-            .json({
-                statusCode: status
-            });
+        super.catch(exception, host);
     }
 }
