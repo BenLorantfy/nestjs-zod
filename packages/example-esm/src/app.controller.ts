0807f1ce2c190d1653d4cafdc0f662176aa39701
Ben <BenLorantfy@users.noreply.github.com>
2025-12-28 15:53:53 -0500

    Switch to tsdown

--- packages/example-esm/src/app.controller.ts
+++ packages/example-esm/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createZodDto, ZodResponse } from 'nestjs-zod';
+import z from 'zod';
+
+class BookDto extends createZodDto(z.object({
+  title: z.string().describe('The title of the book'),
+}).meta({ id: 'Book', title: 'Book' })) {}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/book')
+  @ZodResponse({ type: BookDto })
+  getBooks() {
+    return {
+      title: 'The Martian'
+    }
   }
 }
--- packages/example-esm/src/app.module.ts
+++ packages/example-esm/src/app.module.ts
@@ -1,10 +1,59 @@
-import { Module } from '@nestjs/common';
+import {
+  ZodValidationPipe,
+  ZodSerializerInterceptor,
+  ZodSerializationException,
+} from 'nestjs-zod';
+import {
+  APP_PIPE,
+  APP_INTERCEPTOR,
+  APP_FILTER,
+  BaseExceptionFilter,
+} from '@nestjs/core';
+import { ZodError } from 'zod';
+import {
+  Module,
+  HttpException,
+  ArgumentsHost,
+  Logger,
+  Catch,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+@Catch(HttpException)
+class HttpExceptionFilter extends BaseExceptionFilter {
+  private logger = new Logger(HttpExceptionFilter.name);
+
+  catch(exception: HttpException, host: ArgumentsHost) {
+    if (exception instanceof ZodSerializationException) {
+      const zodError = exception.getZodError();
+
+      if (zodError instanceof ZodError) {
+        this.logger.error(`ZodSerializationException: ${zodError.message}`);
+      }
+    }
+
+    super.catch(exception, host);
+  }
+}
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ZodSerializerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
