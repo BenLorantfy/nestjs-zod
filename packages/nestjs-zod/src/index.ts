39cdc2e970f96778c3f9ed9878faa0ac6d4c9df3
Ben <BenLorantfy@users.noreply.github.com>
2025-12-28 14:06:30 -0500

    Add `strictSchemaDeclaration` param

--- packages/nestjs-zod/src/index.ts
+++ packages/nestjs-zod/src/index.ts
@@ -3,7 +3,7 @@ export { createZodDto } from './dto'
 export { ZodValidationException, ZodSerializationException } from './exception'
 export { createZodGuard, UseZodGuard, ZodGuard } from './guard'
 export { zodV3ToOpenAPI } from './zodV3ToOpenApi'
-export { createZodValidationPipe, ZodValidationPipe } from './pipe'
+export { createZodValidationPipe, ZodValidationPipe, ZodSchemaDeclarationException } from './pipe'
 export { createZodSerializerInterceptor, ZodSerializerDto, ZodSerializerInterceptor } from './serializer'
 export { validate } from './validate'
 export { cleanupOpenApiDoc } from './cleanupOpenApiDoc';
